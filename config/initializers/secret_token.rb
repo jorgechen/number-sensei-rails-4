32d62f2c0005d2ada42c07cb98cc12b91192b7f3
George Chen (Air) <sylvanknave@gmail.com>
2013-07-22 13:27:03 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NumberSenseiRails4::Application.config.secret_key_base = '576a728e65d61a242ab2ad2f05ab28e2c4dda86b5bc34055e8cc3d5b2d29fa7626cdabcef1bd5450ec205b0ce72e92aa457f156ffc1c31beec0cbd284f5f141c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NumberSenseiRails4::Application.config.session_store :cookie_store, key: '_number-sensei-rails-4_session'
