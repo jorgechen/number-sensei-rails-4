12c6c91d0dc2df0815b0d7e44d8591f2ac914bfa
George Chen (Air) <sylvanknave@gmail.com>
2013-08-08 23:50:00 -0500

    enables Hirb pretty text for Rails console

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 NumberSenseiRails4::Application.initialize!
+
+# Rails Console only
+if defined?(Rails::Console)
+  puts 'Automatically enabling Hirb... (in environment.rb)'
+  Hirb.enable
+end
