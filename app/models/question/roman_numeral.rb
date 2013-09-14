d732009ecf1447329fd4680b7a7a12e133e7b576
George Chen (Air) <sylvanknave@gmail.com>
2013-09-14 05:24:47 -0500

    Updated tricks#show to dynamically check problems you do.

--- app/models/question/roman_numeral.rb
+++ app/models/question/roman_numeral.rb
@@ -1,13 +1,30 @@
+# Asks you to convert a Roman numeral to Arabic
 class Question::RomanNumeral < Question
 
-  def html
+  def roman
+    RomanNumeral.to_roman(expression.to_i)
   end
+
+  def arabic
+    expression.to_i
+  end
+
+  def appendix
+    '(Arabic numeral)'
+  end
+
+  def plain_text
+    roman
+  end
+
+  def html
+    roman
+  end
+
 
   def self.build(arabic)
     x = self.new
     x.expression = IntegerExpression.build(arabic)
-
-
     x
   end
 
