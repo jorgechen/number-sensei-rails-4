f5a37da92e77a1f10440fdb6aaef6a66516a88de
George Chen (Air) <sylvanknave@gmail.com>
2014-02-15 10:02:01 -0600

    Utility methods for Question::Division

--- app/models/question/division.rb
+++ app/models/question/division.rb
@@ -5,4 +5,16 @@ class Question::Division < Question::Binary
     DivisionExpression.build left, right
   end
 
+  def dividend
+    expression.left.to_i
+  end
+
+  def divisor
+    expression.right.to_i
+  end
+
+  def quotient
+    answer.to_f #TODO default set to fractions?
+  end
+
 end
