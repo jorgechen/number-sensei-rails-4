d3f373df63e4ffcd348f05380af9345eaedbfeb7
George Chen (Air) <sylvanknave@gmail.com>
2014-02-22 10:46:40 -0600

    Now we can "rake db:seeds:tricks" to seed missing tricks and assign questions to them.

--- db/seeds/tricks.seeds.rb
+++ db/seeds/tricks.seeds.rb
@@ -2,20 +2,15 @@ require 'sylvan_rails/knav_util'
 
 KnavUtil.print_with_padding 'Tricks'
 
-#TODO this is unnecessary if questions are not seeded
-
 # Ensure all tricks exist, and they connect with all applicable questions.
 Strategy.enum_options.each do |s|
-  t = Trick.where(strategy: s.enum).first_or_create!
+  t = Trick.where(strategy: s.enum).first_or_initialize
 
-  #NOTE: Currently we automatically associate tricks upon creating a question
-  ## associate pre-seeded questions with applicable tricks
-  #questions = Question.all
-  #puts "Finding questions for trick \t #{t.name} (#{t.to_s})"
-  #questions.find_each do |q|
-  #  if t.question_qualifies? q
-  #    q.tricks << t rescue nil # ignore if relationship already exists
-  #  end
-  #end
+  if t.new_record? and t.save
+    # NOTE: Currently we automatically associate tricks upon creating a question
+    # associate pre-seeded questions with applicable tricks
+    puts "Finding questions for trick \t #{t.name} (#{t.to_s})"
+    Armory::assign_questions(t)
+  end
 
 end
